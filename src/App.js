c6cde13c9da0649062c5d5ff9cbfba043e95577d
Cray J Thomas Jr <cthomas@buildinmotion.com>
2019-02-23 20:38:46 -0500

    Changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Sleep Sleep!!
+           Look Im Reacting
           </p>
           <a
             className="App-link"
